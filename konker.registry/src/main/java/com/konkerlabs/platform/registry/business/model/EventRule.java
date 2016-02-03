3ac216ac4fab21d6bed3492256c36b54453a0687
Alexandre Cardoso <accbel@gmail.com>
2016-02-03 19:22:33 -0200

    EventRule unit tests

--- konker.registry/src/main/java/com/konkerlabs/platform/registry/business/model/EventRule.java
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/business/model/EventRule.java
@@ -2,6 +2,7 @@ package com.konkerlabs.platform.registry.business.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -26,15 +27,45 @@ public class EventRule {
     private List<RuleTransformation> transformations = new ArrayList<>();
     private boolean active;
 
+    public List<String> applyValidations() {
+        List<String> validations = new ArrayList<>();
+
+        if (getTenant()==null)
+            validations.add("Tenant cannot be null");
+        if (getName()==null || getName().isEmpty())
+            validations.add("Name cannot be null or empty");
+        if (getIncoming()==null)
+            validations.add("Incoming actor cannot be null");
+        if (getIncoming() != null && getIncoming().getUri() == null)
+            validations.add("Incoming actor URI cannot be null");
+        if (getOutgoing()==null)
+            validations.add("Outgoing actor cannot be null");
+        if (getOutgoing() != null && getOutgoing().getUri() == null)
+            validations.add("Outgoing actor URI cannot be null");
+
+        if (validations.isEmpty())
+            return null;
+        else
+            return validations;
+    }
+
     @Data
     public static class RuleActor {
         private URI uri;
         private Map<String,String> data = new HashMap<>();
+
+        public RuleActor(URI uri) {
+            this.uri = uri;
+        }
     }
 
     @Data
     public static class RuleTransformation {
         private String type;
         private Map<String,String> data = new HashMap<>();
+
+        public RuleTransformation(String type) {
+            this.type = type;
+        }
     }
 }
---
+++ konker.registry/src/test/java/com/konkerlabs/platform/registry/test/business/model/EventRuleTest.java
@@ -0,0 +1,103 @@
+package com.konkerlabs.platform.registry.test.business.model;
+
+import com.konkerlabs.platform.registry.business.model.EventRule;
+import com.konkerlabs.platform.registry.business.model.Tenant;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.net.URI;
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.nullValue;
+
+public class EventRuleTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private EventRule subject;
+
+    @Before
+    public void setUp() throws Exception {
+
+        Tenant tenant = Tenant.builder().name("Konker").build();
+
+        subject = EventRule.builder()
+            .tenant(tenant)
+            .name("Rule name")
+            .description("Description")
+            .incoming(new EventRule.RuleActor(new URI("device://0000000000000004/")))
+            .outgoing(new EventRule.RuleActor(new URI("device://0000000000000005/")))
+            .transformations(Arrays.asList(new EventRule.RuleTransformation[]{
+                    new EventRule.RuleTransformation("CONTENT_MATCH")
+            }))
+            .active(true)
+            .build();
+    }
+
+    @Test
+    public void shouldReturnAValidationMessageIfTenantIsNull() throws Exception {
+        subject.setTenant(null);
+
+        String expectedMessage = "Tenant cannot be null";
+
+        assertThat(subject.applyValidations(), hasItem(expectedMessage));
+    }
+    @Test
+    public void shouldReturnAValidationMessageIfNameIsNull() throws Exception {
+        subject.setName(null);
+
+        String expectedMessage = "Name cannot be null or empty";
+
+        assertThat(subject.applyValidations(), hasItem(expectedMessage));
+    }
+    @Test
+    public void shouldReturnAValidationMessageIfNameIsEmpty() throws Exception {
+        subject.setName("");
+
+        String expectedMessage = "Name cannot be null or empty";
+
+        assertThat(subject.applyValidations(), hasItem(expectedMessage));
+    }
+    @Test
+    public void shouldReturnAValidationMessageIfIncomingIsNull() throws Exception {
+        subject.setIncoming(null);
+
+        String expectedMessage = "Incoming actor cannot be null";
+
+        assertThat(subject.applyValidations(), hasItem(expectedMessage));
+    }
+    @Test
+    public void shouldReturnAValidationMessageIfIncomingURIIsNull() throws Exception {
+        subject.getIncoming().setUri(null);
+
+        String expectedMessage = "Incoming actor URI cannot be null";
+
+        assertThat(subject.applyValidations(), hasItem(expectedMessage));
+    }
+    @Test
+    public void shouldReturnAValidationMessageIfOutgoingIsNull() throws Exception {
+        subject.setOutgoing(null);
+
+        String expectedMessage = "Outgoing actor cannot be null";
+
+        assertThat(subject.applyValidations(), hasItem(expectedMessage));
+    }
+    @Test
+    public void shouldReturnAValidationMessageIfOutgoingURIIsNull() throws Exception {
+        subject.getOutgoing().setUri(null);
+
+        String expectedMessage = "Outgoing actor URI cannot be null";
+
+        assertThat(subject.applyValidations(), hasItem(expectedMessage));
+    }
+
+    @Test
+    public void shouldHaveNoValidationMessagesIfRecordIsValid() throws Exception {
+        assertThat(subject.applyValidations(), nullValue());
+    }
+}
