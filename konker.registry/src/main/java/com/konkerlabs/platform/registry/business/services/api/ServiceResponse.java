01c96ecc08669409b272695d0087053c1ea6a390
wmariusso <wmariusso@gmail.com>
2016-02-24 16:52:41 -0300

    Implement Data Enrichment Services

--- konker.registry/src/main/java/com/konkerlabs/platform/registry/business/services/api/ServiceResponse.java
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/business/services/api/ServiceResponse.java
@@ -2,6 +2,7 @@ package com.konkerlabs.platform.registry.business.services.api;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Singular;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class ServiceResponse<T> {
     public enum Status { OK, ERROR }
 
     private Status status;
+    @Singular
     private List<String> responseMessages;
     private T result;
 }
