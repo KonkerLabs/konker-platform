47d70f00318512d6cf0a44a5ccfb1214b71201a6
wmariusso <wmariusso@gmail.com>
2016-03-21 22:28:03 -0300

    Fixing tests - Injecting mock user into Testing context

--- konker.registry/src/test/java/com/konkerlabs/platform/registry/test/base/WebTestConfiguration.java
+++ konker.registry/src/test/java/com/konkerlabs/platform/registry/test/base/WebTestConfiguration.java
@@ -1,6 +1,8 @@
 package com.konkerlabs.platform.registry.test.base;
 
 import com.konkerlabs.platform.registry.business.model.Tenant;
+import com.konkerlabs.platform.registry.business.model.User;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,11 @@ public class WebTestConfiguration {
     @Bean
     public Tenant tenant() {
         return Tenant.builder().name("konker").domainName("konker").id("id").build();
+    }
+
+    @Bean
+    public User user() {
+        return User.builder().email("user@domain.com").tenant(tenant()).build();
     }
 
 }
