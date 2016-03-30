fedf56441c98de47abe5f78f7529a6768274f479
Alexandre Cardoso <accbel@gmail.com>
2016-03-30 11:19:08 -0300

    Just a few adjustments

--- konker.samples/smart-temperature/webapp.py
+++ konker.samples/smart-temperature/webapp.py
@@ -1,18 +1,18 @@
 from flask import *
 import json
-import service
+from service import pid as pid_service
 
 app = Flask(__name__)
 
 @app.route('/pid/control', methods=['POST'])
-def control():
+def pid_control():
     if not request.args.get('key',None):
         abort(400, "Key must be provided!")
     if not request.args.get('input',None):
         abort(400, "Input value must be provided!")
 
     sp = request.args.get('Sp',None)
-    result = service.control(
+    result = pid_service.control(
         key=request.args['key'],
         input=float(request.args['input']),
         Sp=float(sp) if sp else None
@@ -22,8 +22,8 @@ def control():
     return jsonify(response)
 
 @app.route('/pid/set', methods=['POST'])
-def set_params():
-    service.set(key=request.args['key'],kp=request.args['kp'],ki=request.args['ki'],kd=request.args['kd'])
+def pid_set_params():
+    pid_service.set(key=request.args['key'],kp=request.args['kp'],ki=request.args['ki'],kd=request.args['kd'])
     return ""
 
 if __name__ == '__main__':
