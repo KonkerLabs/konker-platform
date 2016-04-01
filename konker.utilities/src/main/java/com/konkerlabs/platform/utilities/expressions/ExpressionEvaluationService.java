1179a40e120d230fdb463beb588499b6d9c2206b
Alexandre Cardoso <accbel@gmail.com>
2016-04-01 12:21:57 -0300

    expression evaluation regex bug fix

--- konker.utilities/src/main/java/com/konkerlabs/platform/utilities/expressions/ExpressionEvaluationService.java
+++ konker.utilities/src/main/java/com/konkerlabs/platform/utilities/expressions/ExpressionEvaluationService.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public interface ExpressionEvaluationService {
 
-    Pattern EXPRESSION_TEMPLATE_PATTERN = Pattern.compile(".*\\@\\{.*}");
+    Pattern EXPRESSION_TEMPLATE_PATTERN = Pattern.compile(".*\\@\\{.*}.*");
 
     String evaluateTemplate(String expressionTemplate, Map<String, Object> evaluationContext);
     boolean evaluateConditional(String conditionalExpression, Map<String, Object> evaluationContext);
