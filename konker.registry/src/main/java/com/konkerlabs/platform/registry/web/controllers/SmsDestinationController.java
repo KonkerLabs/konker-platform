6da6ab4feeed7a31262957f14ef8c77464f641d2
Douglas Apolinário <dapolina@inmetrics.com.br>
2016-09-19 17:24:34 -0300

    add feature flag to enable or disable SMS

--- konker.registry/src/main/java/com/konkerlabs/platform/registry/web/controllers/SmsDestinationController.java
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/web/controllers/SmsDestinationController.java
@@ -1,16 +1,20 @@
 package com.konkerlabs.platform.registry.web.controllers;
 
-import com.konkerlabs.platform.registry.business.model.SmsDestination;
-import com.konkerlabs.platform.registry.business.model.Tenant;
-import com.konkerlabs.platform.registry.business.services.api.NewServiceResponse;
-import com.konkerlabs.platform.registry.business.services.api.SmsDestinationService;
-import com.konkerlabs.platform.registry.web.forms.SmsDestinationForm;
+import static java.text.MessageFormat.format;
+
+import java.util.Locale;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +23,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Locale;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
-import static java.text.MessageFormat.format;
+import com.konkerlabs.platform.registry.business.model.SmsDestination;
+import com.konkerlabs.platform.registry.business.model.Tenant;
+import com.konkerlabs.platform.registry.business.services.api.NewServiceResponse;
+import com.konkerlabs.platform.registry.business.services.api.SmsDestinationService;
+import com.konkerlabs.platform.registry.web.forms.SmsDestinationForm;
 
 @Controller
 @Scope("request")
