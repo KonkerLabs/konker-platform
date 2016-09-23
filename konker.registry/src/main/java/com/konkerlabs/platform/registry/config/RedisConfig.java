c4d40473a1a129cad9ae6b1c266369b4d9f313d7
Andre Rocha <andre@konkerlabs.com>
2016-09-23 15:17:29 -0300

    [KRMVP-280][WIP] Redis connection pool

---
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.konkerlabs.platform.registry.config;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Factory for Redis connections
+ * Using Jedis as a driver
+ * Created by andre on 23/09/16.
+ * @author andre
+ * @since 2016-09-23
+ */
+@Configuration
+public class RedisConfig {
+
+    public static Config config = ConfigFactory.load().getConfig("redis");
+
+    @Bean
+    public JedisConnectionFactory redisFactory() {
+        JedisConnectionFactory cf = new JedisConnectionFactory();
+        cf.setHostName(config.getString("master.host"));
+        cf.setPort(config.getInt("master.port"));
+        cf.setUsePool(true);
+        return cf;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> rt = new RedisTemplate<>();
+        rt.setConnectionFactory(redisFactory());
+        return rt;
+    }
+
+}
---
+++ konker.registry/src/test/java/com/konkerlabs/platform/registry/test/base/RedisTestConfiguration.java
@@ -0,0 +1,30 @@
+package com.konkerlabs.platform.registry.test.base;
+
+import com.konkerlabs.platform.registry.config.RedisConfig;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Created by andre on 23/09/16.
+ */
+@Configuration
+public class RedisTestConfiguration extends RedisConfig {
+
+    @Override
+    @Bean
+    public JedisConnectionFactory redisFactory() {
+        return Mockito.mock(
+                JedisConnectionFactory.class,
+                Mockito.withSettings().extraInterfaces(DisposableBean.class));
+    }
+
+    @Override
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        return super.redisTemplate();
+    }
+}
