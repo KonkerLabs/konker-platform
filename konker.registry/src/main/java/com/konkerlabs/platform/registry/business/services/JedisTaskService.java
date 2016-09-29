f56d147b46086c1fcac339cc3a195f47a3764902
Douglas Apolinário <dapolina@inmetrics.com.br>
2016-09-29 10:58:54 -0300

    KRMVP-225 - change from deviceId to guid.

--- konker.registry/src/main/java/com/konkerlabs/platform/registry/business/services/JedisTaskService.java
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/business/services/JedisTaskService.java
@@ -38,7 +38,7 @@ public class JedisTaskService {
 			@Override
 			public void onMessage(String channel, String message) {
 				Device device = deviceRegisterService.findByApiKey(channel.substring(0, channel.indexOf(".")));
-				response = deviceEventService.findOutgoingBy(device.getTenant(), device.getDeviceId(),
+				response = deviceEventService.findOutgoingBy(device.getTenant(), device.getGuid(),
 						startTimestamp, null, asc, limit);
 				this.unsubscribe(channel);
 			}
