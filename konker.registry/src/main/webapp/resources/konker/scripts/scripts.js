42c1f3220de42dba92335f0b0cd670b82a53dbff
Douglas Apolinário <dapolina@inmetrics.com.br>
2016-10-07 15:15:11 -0300

    change login page style

---
+++ konker.registry/src/main/webapp/resources/konker/scripts/scripts.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("/resources/konker/images/login-background.jpg");
+    
+    /*
+        Form validation
+    */
+    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.login-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    
+});
