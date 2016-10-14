47e6e92890bad4ad3d81df85cb6ca8265c93afb6
wmariusso <wmariusso@gmail.com>
2016-10-14 20:39:33 -0300

    Logging information about users trying to log on

--- konker.registry/src/main/java/com/konkerlabs/platform/registry/security/TenantUserDetailsService.java
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/security/TenantUserDetailsService.java
@@ -1,6 +1,10 @@
 package com.konkerlabs.platform.registry.security;
 
 import com.konkerlabs.platform.registry.business.repositories.UserRepository;
+import com.konkerlabs.platform.registry.config.SecurityConfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,11 +16,14 @@ import java.util.Optional;
 @Service("tenantUserDetails")
 public class TenantUserDetailsService implements UserDetailsService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TenantUserDetailsService.class);
+    
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        LOGGER.info("Loading details for username " + email);
         return Optional
             .ofNullable(userRepository.findOne(Optional.of(email).orElse("").trim().toLowerCase()))
             .orElseThrow(() -> new UsernameNotFoundException("authentication.credentials.invalid"));
