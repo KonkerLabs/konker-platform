f70261f8ea74c9eafd6d0d15040eabec15d0f12e
Andre Rocha <andre@konkerlabs.com>
2016-12-22 18:08:06 -0200

    fix handler for tests

--- konker.registry/src/main/java/com/konkerlabs/platform/registry/interceptor/RequestHandlerInterceptor.java
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/interceptor/RequestHandlerInterceptor.java
@@ -2,6 +2,8 @@ package com.konkerlabs.platform.registry.interceptor;
 
 import com.konkerlabs.platform.registry.business.model.User;
 import com.konkerlabs.platform.registry.security.UserContextResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,24 +20,31 @@ import java.util.Optional;
 
 public class RequestHandlerInterceptor extends HandlerInterceptorAdapter {
 
+    private Logger LOG = LoggerFactory.getLogger(RequestHandlerInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication()).isPresent() &&
                 SecurityContextHolder.getContext().getAuthentication().getPrincipal()
                 instanceof UserDetails) {
-            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            if (Optional.ofNullable(request.getSession()).isPresent()
-                    && Optional.ofNullable(user).isPresent()) {
-                if (!Optional.ofNullable(request.getSession()
-                        .getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME)).isPresent() ||
-                        !request.getSession().getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME)
-                                .equals(user.getLanguage().getLocale())) {
+            try {
+                User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                if (Optional.ofNullable(request.getSession()).isPresent()
+                        && Optional.ofNullable(user).isPresent()) {
+                    if (!Optional.ofNullable(request.getSession()
+                            .getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME)).isPresent() ||
+                            !request.getSession().getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME)
+                                    .equals(user.getLanguage().getLocale())) {
 
-                    request.getSession().setAttribute(
-                            SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME,
-                            user.getLanguage().getLocale());
+                        request.getSession().setAttribute(
+                                SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME,
+                                user.getLanguage().getLocale());
+                    }
                 }
+            } catch (ClassCastException e){
+                LOG.error("Invalid user type on session");
             }
+
         }
 
         return super.preHandle(request, response, handler);
