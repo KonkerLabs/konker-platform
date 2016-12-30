5a2f18d9186c94c22d6cd645d9868f68b6b17506
wmariusso <wmariusso@gmail.com>
2016-12-30 15:35:18 -0200

    Adding Notifications Screen

--- konker.registry/src/main/java/com/konkerlabs/platform/registry/business/model/enumerations/DateFormat.java
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/business/model/enumerations/DateFormat.java
@@ -1,9 +1,12 @@
 package com.konkerlabs.platform.registry.business.model.enumerations;
 
+import java.time.format.DateTimeFormatter;
+
 public enum DateFormat {
-    DDMMYYYY("dateformats.ddmmyyyy", "dd/MM/yyyy"),
-    YYYYMMDD("dateformats.yyyymmdd", "yyyy/MM/dd"),
-    MMDDYYYY("dateformats.mmddyyyy", "MM/dd/yyyy");
+    DDMMYYYY("dateformats.ddmmyyyy", "dd/MM/yyyy"), YYYYMMDD("dateformats.yyyymmdd",
+            "yyyy/MM/dd"), MMDDYYYY("dateformats.mmddyyyy", "MM/dd/yyyy");
+
+    private static final String DEFAULT_HOUR_FORMAT = "HH:mm:ss";
 
     private String code;
     private String id;
@@ -17,7 +20,16 @@ public enum DateFormat {
         return id;
     }
 
-    public String getCode(){
+    public String getCode() {
         return code;
     }
+
+    public DateTimeFormatter getDateFormatter() {
+        return DateTimeFormatter.ofPattern(this.id);
+    }
+
+    public DateTimeFormatter getDateTimeFormatter() {
+        return DateTimeFormatter.ofPattern(this.id + " " + DEFAULT_HOUR_FORMAT);
+    }
+
 }
