968db32e210dcdfea4fdaa2ee8e65495a9923f18
Felipe Dias <felipe@konkerlabs.com>
2017-01-09 17:27:14 -0200

    [KRMVP-353] - Json formatter refactored to support undo pretty json - Undo pretty json option added to event and visualization views - Undo pretty json strings added to global resource bundle

--- konker.registry/src/main/webapp/resources/konker/scripts/json.formatter.js
+++ konker.registry/src/main/webapp/resources/konker/scripts/json.formatter.js
@@ -1,12 +1,26 @@
-$.each($(".json-data"), function () {
-        var payload = $(this).html();
-        var obj = JSON.parse(payload);
+var toggle = false;
+$("#isJsonFormatted").click(function () {
+    $("input[type=checkbox]").attr("checked", !toggle);
+    toggle = !toggle;
+    formatJson(toggle);
+});
 
-        var node = new PrettyJSON.view.Node({
-        el:$(this),
-        data:obj
-        });
-
-        node.expandAll();
-    }
-);
+function formatJson(isFormatted) {
+    $.each($(".json-data"), function () {
+            var payload = $(this).html();
+            if (isFormatted) {
+                var obj = JSON.parse(payload);
+                $(this).attr("text", payload);
+                var node = new PrettyJSON.view.Node({
+                    el: $(this),
+                    data: obj
+                });
+                node.expandAll();
+            }
+            else {
+                var old = $(this).attr("text");
+                $(this).html(old);
+            }
+        }
+    );
+}
