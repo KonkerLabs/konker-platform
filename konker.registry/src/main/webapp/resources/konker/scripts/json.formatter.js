5dcdd5e6fb495ae57ddec66f2c65cb522590d182
Felipe Dias <felipe@konkerlabs.com>
2017-01-09 19:37:18 -0200

    [KRMVP-353] - Toggle button added to handle json formatter - json formatter refactored to handle online data

--- konker.registry/src/main/webapp/resources/konker/scripts/json.formatter.js
+++ konker.registry/src/main/webapp/resources/konker/scripts/json.formatter.js
@@ -1,6 +1,6 @@
 var toggle = false;
 $("#isJsonFormatted").click(function () {
-    $("input[type=checkbox]").attr("checked", !toggle);
+    $("#isJsonFormatted").attr("checked", !toggle);
     toggle = !toggle;
     formatJson(toggle);
 });
@@ -9,17 +9,21 @@ function formatJson(isFormatted) {
     $.each($(".json-data"), function () {
             var payload = $(this).html();
             if (isFormatted) {
-                var obj = JSON.parse(payload);
+                try {
+                    var result = JSON.parse(payload);
+                } catch (e) {
+
+                }
                 $(this).attr("text", payload);
                 var node = new PrettyJSON.view.Node({
                     el: $(this),
-                    data: obj
+                    data: result
                 });
                 node.expandAll();
             }
             else {
-                var old = $(this).attr("text");
-                $(this).html(old);
+                var unformattedJSON = $(this).attr("text");
+                $(this).html(unformattedJSON);
             }
         }
     );
