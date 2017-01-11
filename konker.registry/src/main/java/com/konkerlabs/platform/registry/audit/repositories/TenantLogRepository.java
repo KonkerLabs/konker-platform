b747c2d00adc2b6f2215aaab9b3cbe81eae8c483
ckobayashi <ckobayashi@konkerlabs.com>
2017-01-11 16:18:03 -0200

    [KRMVP-377] Add TenantLogRepository.getInstance() to be used in LogAppender

--- konker.registry/src/main/java/com/konkerlabs/platform/registry/audit/repositories/TenantLogRepository.java
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/audit/repositories/TenantLogRepository.java
@@ -4,13 +4,19 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.experimental.theories.Theories;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.CollectionOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
+import org.w3c.dom.html.HTMLIsIndexElement;
 
 import com.konkerlabs.platform.registry.audit.model.TenantLog;
 import com.konkerlabs.platform.registry.business.model.Tenant;
+import com.konkerlabs.platform.registry.config.MongoAuditConfig;
+import com.konkerlabs.platform.registry.interceptor.RequestHandlerInterceptor;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -19,11 +25,28 @@ import com.mongodb.DBObject;
 @Repository
 public class TenantLogRepository {
 
+	private static Logger LOG = LoggerFactory.getLogger(TenantLogRepository.class);
+
 	@Autowired
 	private MongoTemplate mongoAuditTemplate;
 
 	public static final int MAX_DOCUMENTS = 1000;
 
+	public static TenantLogRepository getInstance() {
+
+		try {
+			TenantLogRepository tenantLogRepository = new TenantLogRepository();
+			tenantLogRepository.mongoAuditTemplate = new MongoAuditConfig().mongoTemplate();
+
+			return tenantLogRepository;
+		} catch (Exception e) {
+			LOG.error("Exception while creating TenantLogRepository instance", e);
+
+			return null;
+		}
+		
+	}
+	
 	public List<TenantLog> findAll(Tenant tenant) {
 
 		List<TenantLog> logs = new ArrayList<>(MAX_DOCUMENTS);
