a66e8a18bf0d7e4261a36d14870471e5bfd962e5
Felipe Dias <felipe@konkerlabs.com>
2017-01-16 18:43:11 -0200

    [KRMVP-381] - script location fixed - sys exit statuses refactored - dsl link name modified - copy command added to dao folder

--- konker.scripts/registry/setup.py
+++ konker.scripts/registry/setup.py
@@ -1,4 +1,4 @@
 #! /usr/bin/env python2
 from setuptools import setup
 
-setup(name='konker', author='KonkerLabs', scripts=['/usr/bin/konker'])
+setup(name='konker', author='KonkerLabs', scripts=['/usr/bin/dsl.py'])
