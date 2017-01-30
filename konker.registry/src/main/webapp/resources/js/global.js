8e2325461b5ba46ec3c1dc7cdce72d88b19058fa
ckobayashi <ckobayashi@konkerlabs.com>
2017-01-30 17:10:47 -0200

    KRMVP-432 - CSS only solution

--- konker.registry/src/main/webapp/resources/js/global.js
+++ konker.registry/src/main/webapp/resources/js/global.js
@@ -1,30 +1,3 @@
 $(document).ready( function () {
 	$(".switch input[type=hidden]").before($('<div class="slider round"></div>'));
 });
-
-/* footer */
-
-function locateFooter() {
-    /* footer */
-    var windowHeight = $(window).height();
-    var docHeight = $('.wrapper').height();
-    var headerHeight = $('.header').height();
-
-    console.log('windowHeight: ' + windowHeight);
-    console.log('docHeight: ' + docHeight);
-    console.log('headerHeight: ' + headerHeight);
-
-    if (windowHeight > docHeight + headerHeight + 80) {
-        $('#footer').css('bottom', '0px');
-    } else {
-        $('#footer').css('bottom', 'auto');
-    };
-}
-
-$(document).ready(function() {
-    locateFooter();
-});
-
-$(window).resize(function() {
-    locateFooter();
-});
