cf57f07da33348c8bc2784d131f327abcde8c1bc
ckobayashi <ckobayashi@konkerlabs.com>
2017-02-03 19:07:17 -0200

    KRMVP-398 - Improve Chart Lines

--- konker.registry/src/main/webapp/resources/js/visualization/outlier.js
+++ konker.registry/src/main/webapp/resources/js/visualization/outlier.js
@@ -1,81 +1,57 @@
-function get_segments(data) {
-    var segments = {};
-    if (data.length > 1){
-        var previousData = null;
-        for (var i = data.length - 1; i >= 0; i--) {
-            var currentData = data[i];
-            if (previousData == null) {
-                previousData = currentData;
-            } else {
-                var segment = currentData["timestamp"] - previousData["timestamp"];
-                segments[segment] = {
-                    "d1": previousData,
-                    "d2": currentData
-                };
-                previousData = currentData;
-            }
-        }
+function getSegments(data) {
+    var segments = [];
+    
+    for (var i = 1; i < data.length; i++) {
+        segments.push(data[i - 1]["timestamp"] - data[i]["timestamp"]);
+        console.log(data[i - 1]["timestamp"] - data[i]["timestamp"]);
     }
+
+    segments.sort(function(a,b) { return a - b; });
+
     return segments;
 }
 
-function get_outliers(segments) {
-    var median = math.median(Object.keys(segments));
-    var values = Object.keys(segments)
-    var q1 = d3.quantile(values, 0.25);
+function getOutlierLimit(values) {
+    var median = d3.median(values);
     var q3 = d3.quantile(values, 0.75);
-    var iqr = q3 - q1;
-    var outlier = median + 2.5 * iqr;
 
-    var keys = Object.keys(segments);
-    var outliers = {};
-    for (var i = 0; i <  keys.length; i++) {
-        if (keys[i] > outlier) {
-            outliers[keys[i]] = segments[keys[i]];
-        }
+    if (values.length < 10) {
+        // no outliers: small sample
+        outlier = d3.max(values) + median; 
+    } else {
+        outlier = median + 10.0 * q3;
     }
 
-    return outliers;
+    return outlier;
 }
 
 function data_filter(data) {
-    var segments = get_segments(data);
-    var outliers = get_outliers(segments);
-    return filter(outliers, data);
+    var segments = getSegments(data);
+    var outlierLimit = getOutlierLimit(segments);
+    return filter(outlierLimit, data);
 }
 
-function filter(outliers, data) {
+function filter(outlierLimit, data) {
     var result = []
-
     var set = [];
-    var previousIsOutlier = false;
-    for (var i = 0; i < data.length; i++) {
-        if (i < data.length - 1) {
-            var distance =  data[i]["timestamp"] - data[i + 1]["timestamp"];
-            if (outliers[distance] != undefined) {
-                if (!previousIsOutlier) {
-                    set.push(data[i]);
-                    result.push(set);
-                    set = [];
-                } else {
-                    result.push(set);
-                    set = [];
 
-                    // last outlier
-                    if (i == data.length - 2){
-                        result.push([data[i]]);
-                        result.push([data[i + 1]]);
-                    }
-                }
-                set.push(data[i + 1]);
-                i = i + 1;
-                previousIsOutlier = true;
+    set.push(data[0]);
+    result.push(set);
+
+    for (var i = 1; i < data.length; i++) {
+        if (i < data.length - 1) {
+            var distance =  data[i - 1]["timestamp"] - data[i]["timestamp"];
+            if (distance > outlierLimit) {
+                set = [];
+                set.push(data[i]);
+                result.push(set);
             } else {
                 set.push(data[i]);
-                previousIsOutlier = false;
             }
         }
     }
 
+    console.log('results: ' + result.length);
+    
     return result;
 }
