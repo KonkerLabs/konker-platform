d259235ecbcc92ebc7610753de76164e32087f72
dapolina <doliveira.apolinario@gmail.com>
2017-02-03 16:00:00 -0200

    KRMVP-453 - device authentication fails

--- konker.registry/src/main/java/com/konkerlabs/platform/registry/security/KonkerBasicAuthenticationFilter.java
+++ konker.registry/src/main/java/com/konkerlabs/platform/registry/security/KonkerBasicAuthenticationFilter.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
+@Component("customBasicAuthFilter")
 public class KonkerBasicAuthenticationFilter extends BasicAuthenticationFilter {
 
+	@Autowired
 	public KonkerBasicAuthenticationFilter(AuthenticationManager authenticationManager) {
 		super(authenticationManager);
 	}
