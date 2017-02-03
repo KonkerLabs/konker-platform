c2bdf49be986cc89dd9ae3ffea9aa5e4436d70c7
Andre Rocha <andre@konkerlabs.com>
2017-02-03 16:40:00 -0200

    improve mongo sleep script to avoid infinity loop start

--- konker.scripts/docker-demo/build/mongodb/sleepstart.sh
+++ konker.scripts/docker-demo/build/mongodb/sleepstart.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
-MONGOISREADY=`nc 127.0.0.1 27017 </dev/null; echo $?`
-echo $MONGOISREADY
+MAXRETRY=10;
+RETRYCOUNT=0;
+MONGOISREADY=`nc 127.0.0.1 27017 </dev/null; echo $?`;
+
 while [ $MONGOISREADY != 0 ]
 do
-  echo "waiting for mongo..."$MONGOISREADY
-  MONGOISREADY=`nc 127.0.0.1 27017 </dev/null; echo $?`
-  sleep 10;
+  if [ "$RETRYCOUNT" -lt "$MAXRETRY" ]; then
+    MONGOISREADY=`nc 127.0.0.1 27017 </dev/null; echo $?`;
+    if [ $MONGOISREADY == 0 ]; then
+      echo 'Mongo is ready!';
+    else
+      echo "Waiting for mongo..."$RETRYCOUNT" of "$MAXRETRY;
+      sleep 10;
+      RETRYCOUNT=$(($RETRYCOUNT+1));
+    fi
+  else
+    sleep 5;
+    MONGOISREADY=0;
+    echo "Max tries("$MAXRETRY") was reached, Mongodb is unstable..stop container...";
+    halt;
+  fi
 done
-echo 'mongo is ready!'
