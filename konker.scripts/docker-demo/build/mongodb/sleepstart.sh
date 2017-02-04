cf7c910f55f1a5fae217003197dcc9ed933b6957
Andre Rocha <andre@konkerlabs.com>
2017-02-04 14:40:04 -0200

    fix descriptions

--- konker.scripts/docker-demo/build/mongodb/sleepstart.sh
+++ konker.scripts/docker-demo/build/mongodb/sleepstart.sh
@@ -8,16 +8,16 @@ do
   if [ "$RETRYCOUNT" -lt "$MAXRETRY" ]; then
     MONGOISREADY=`nc 127.0.0.1 27017 </dev/null; echo $?`;
     if [ $MONGOISREADY == 0 ]; then
-      echo 'Mongo is ready!';
+      echo 'Konker Database is ready!';
     else
-      echo "Waiting for mongo..."$RETRYCOUNT" of "$MAXRETRY;
+      echo "Waiting for konker database..."$RETRYCOUNT" of "$MAXRETRY;
       sleep 10;
       RETRYCOUNT=$(($RETRYCOUNT+1));
     fi
   else
     sleep 5;
     MONGOISREADY=0;
-    echo "Max tries("$MAXRETRY") was reached, Mongodb is unstable..stop container...";
+    echo "Max tries("$MAXRETRY") was reached, Konker database is unstable..stop container...";
     halt;
   fi
 done
