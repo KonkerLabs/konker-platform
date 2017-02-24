1e898b10da94dacfd93ac06d8158b69c3e96e126
Andre Rocha <andre@konkerlabs.com>
2017-02-24 18:04:57 -0300

    add swagger base config for docummenting

--- konker.registry.api/src/main/java/com/konkerlabs/platform/registry/api/web/controller/ProbeController.java
+++ konker.registry.api/src/main/java/com/konkerlabs/platform/registry/api/web/controller/ProbeController.java
@@ -1,34 +1,28 @@
 package com.konkerlabs.platform.registry.api.web.controller;
 
+import com.konkerlabs.platform.registry.api.model.ProbeRest;
+import io.swagger.annotations.SwaggerDefinition;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping(value = "/probe")
-@EnableAutoConfiguration
 public class ProbeController {
 
-    @ResponseBody
     @GetMapping
-    public Probe get() {
-        return Probe.builder()
+    public @ResponseBody ProbeRest get() {
+        return ProbeRest.builder()
                 .webcontext(APP_STATUS.OK)
                 .services(APP_STATUS.OK)
                 .timestamp(System.currentTimeMillis())
                 .build();
-    }
-
-    @Builder
-    @Data
-    public static class Probe {
-        private APP_STATUS webcontext;
-        private APP_STATUS services;
-        private Long timestamp;
     }
 
     public static enum APP_STATUS {
