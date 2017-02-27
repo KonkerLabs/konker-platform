cf337f0bf2e37c359784cd6d5505c31e5ca16385
dapolina <doliveira.apolinario@gmail.com>
2017-02-27 16:33:45 -0300

    KRMVP-475 - secure API with oauth2

--- konker.registry.api/src/main/java/com/konkerlabs/platform/registry/api/config/oauth/OAuth2Config.java
+++ konker.registry.api/src/main/java/com/konkerlabs/platform/registry/api/config/oauth/OAuth2Config.java
@@ -2,24 +2,21 @@ package com.konkerlabs.platform.registry.api.config.oauth;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
 
-import com.konkerlabs.platform.registry.business.model.User;
-import com.konkerlabs.platform.registry.business.services.api.ServiceResponse;
-import com.konkerlabs.platform.registry.business.services.api.UserService;
 import com.konkerlabs.platform.security.exceptions.SecurityException;
 import com.konkerlabs.platform.security.managers.PasswordManager;
 
@@ -33,7 +30,8 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 	private AuthenticationManager authenticationManager;
 	
 	@Autowired
-	private UserService userService;
+	@Qualifier("oauth2ClientDetails")
+	private ClientDetailsService clientDetailsService;
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints)
@@ -43,21 +41,7 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		ServiceResponse<List<User>> findByEmail = userService.findAll();
-		List<User> users = findByEmail.getResult();
-		
-		InMemoryClientDetailsServiceBuilder inMemory = clients.inMemory();
-		
-		users.forEach(user -> {
-			inMemory
-				.withClient(user.getEmail())
-				.secret(user.getPassword())
-				.authorizedGrantTypes("client_credentials", "password")
-				.authorities(getAuthorities(user))
-				.scopes("read")
-				.resourceIds(ResourceServer.RESOURCE_ID)
-				.accessTokenValiditySeconds(0);
-		});
+		clients.withClientDetails(clientDetailsService);
 	}
 
 	@Override
@@ -86,12 +70,4 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 		});
 	}
 	
-	private String getAuthorities(User user) {
-		StringBuffer sb = new StringBuffer();
-		
-		user.getAuthorities().forEach(auth -> sb.append(auth.getAuthority() + ","));
-		sb.deleteCharAt(sb.lastIndexOf(","));
-		
-		return sb.toString();
-	}
 }
