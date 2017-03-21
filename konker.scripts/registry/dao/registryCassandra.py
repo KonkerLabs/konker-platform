8639f460cb98f6265d654a4bba6282acd1e2b6bd
dapolina <doliveira.apolinario@gmail.com>
2017-03-21 17:32:32 -0300

    added the deleted field in migration and blank string to the no exists deviceId field

--- konker.scripts/registry/dao/registryCassandra.py
+++ konker.scripts/registry/dao/registryCassandra.py
@@ -28,14 +28,15 @@ def save_incoming_events(incomingEvents, host):
         try:
             db.execute_async(
                 """
-                INSERT INTO incoming_events (device_guid, timestamp, channel, device_id, payload, tenant_domain)
-                VALUES (%s, %s, %s, %s, %s, %s)
+                INSERT INTO incoming_events (device_guid, timestamp, channel, device_id, payload, tenant_domain, deleted)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
                 """, (event['incoming']['deviceGuid'],
                       event['ts'],
                       event['incoming']['channel'],
-                      event['incoming']['deviceId'],
+                      event['incoming']['deviceId'] if event['incoming'].has_key('deviceId') else '',
                       event['payload'],
-                      event['incoming']['tenantDomain'])
+                      event['incoming']['tenantDomain'],
+                      event['deleted'] if event.has_key('deleted') else False)
             )
             
         except Exception as e:
@@ -49,14 +50,15 @@ def save_outgoing_events(outgoingEvents, host):
         try:
             db.execute_async(
                 """
-                INSERT INTO outgoing_events (device_guid, timestamp, channel, device_id, payload, tenant_domain, incoming)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                INSERT INTO outgoing_events (device_guid, timestamp, channel, device_id, payload, tenant_domain, deleted, incoming)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
                 """, (event['outgoing']['deviceGuid'], 
                       event['ts'], 
                       event['outgoing']['channel'],
-                      event['outgoing']['deviceId'], 
+                      event['outgoing']['deviceId'] if event['outgoing'].has_key('deviceId') else '', 
                       event['payload'], 
                       event['outgoing']['tenantDomain'],
+                      event['deleted'] if event.has_key('deleted') else False,
                       json.dumps(event['incoming'], ensure_ascii=False))
             )
             
@@ -69,14 +71,14 @@ def find_incomingEvents_by_timestamp(timestamp, tenant, host):
     try:
         if tenant is None:
             query = """
-                SELECT device_guid, timestamp, channel, device_id, payload, tenant_domain FROM 
+                SELECT device_guid, timestamp, channel, device_id, payload, tenant_domain, deleted FROM 
                     incoming_events WHERE timestamp >= {param1} ALLOW FILTERING
             """
             incomingEvents = db.execute(query.format(param1=str(timestamp)))
             
         else :
             query = """
-                SELECT device_guid, timestamp, channel, device_id, payload, tenant_domain FROM 
+                SELECT device_guid, timestamp, channel, device_id, payload, tenant_domain, deleted FROM 
                     incoming_events WHERE timestamp >= {param1} and tenant_domain = '{param2}' ALLOW FILTERING
             """
             incomingEvents = db.execute(query.format(param1=str(timestamp), param2=str(tenant)))
@@ -91,14 +93,14 @@ def find_outgoingEvents_by_timestamp(timestamp, tenant, host):
     try:
         if tenant is None:
             query = """
-                SELECT device_guid, timestamp, channel, device_id, payload, tenant_domain, incoming FROM 
+                SELECT device_guid, timestamp, channel, device_id, payload, tenant_domain, deleted, incoming FROM 
                     outgoing_events WHERE timestamp >= {param1} ALLOW FILTERING
             """
             outgoingEvents = db.execute(query.format(param1=str(timestamp)))
             
         else :
             query = """
-                SELECT device_guid, timestamp, channel, device_id, payload, tenant_domain, incoming FROM 
+                SELECT device_guid, timestamp, channel, device_id, payload, tenant_domain, deleted, incoming FROM 
                     outgoing_events WHERE timestamp >= {param1} and tenant_domain = '{param2}' ALLOW FILTERING
             """
             outgoingEvents = db.execute(query.format(param1=str(timestamp), param2=str(tenant)))
@@ -121,6 +123,7 @@ def create_incoming_events_table(host):
                 timestamp bigint,
                 device_id text,
                 payload text,
+                deleted boolean,
                 PRIMARY KEY (device_guid, tenant_domain, channel, timestamp)
             ) WITH CLUSTERING ORDER BY ( tenant_domain ASC, channel ASC, timestamp DESC )
             """
@@ -143,6 +146,7 @@ def create_outgoing_events_table(host):
                 timestamp bigint,
                 device_id text,
                 payload text,
+                deleted boolean,
                 incoming text,
                 PRIMARY KEY (device_guid, tenant_domain, channel, timestamp)
             ) WITH CLUSTERING ORDER BY ( tenant_domain ASC, channel ASC, timestamp DESC )
