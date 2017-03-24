b2580c54e8366de35c245b26a5befbec759caf99
Andre Rocha <andre@konkerlabs.com>
2017-03-24 08:51:05 -0300

    avoid startup error

--- konker.registry.api/src/main/java/com/konkerlabs/platform/registry/api/config/CassandraApiConfig.java
+++ konker.registry.api/src/main/java/com/konkerlabs/platform/registry/api/config/CassandraApiConfig.java
@@ -4,6 +4,5 @@ import com.konkerlabs.platform.registry.config.CassandraConfig;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
 public class CassandraApiConfig extends CassandraConfig  {
 }
