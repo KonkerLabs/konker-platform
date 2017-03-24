4007e90a4e0612cb91dd306bb6741581a6b58a29
ckobayashi <ckobayashi@konkerlabs.com>
2017-03-24 14:58:33 -0300

    add @Configuration at CassandraApiConfig

--- konker.registry.api/src/main/java/com/konkerlabs/platform/registry/api/config/CassandraApiConfig.java
+++ konker.registry.api/src/main/java/com/konkerlabs/platform/registry/api/config/CassandraApiConfig.java
@@ -3,6 +3,6 @@ package com.konkerlabs.platform.registry.api.config;
 import com.konkerlabs.platform.registry.config.CassandraConfig;
 import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class CassandraApiConfig extends CassandraConfig  {
 }
