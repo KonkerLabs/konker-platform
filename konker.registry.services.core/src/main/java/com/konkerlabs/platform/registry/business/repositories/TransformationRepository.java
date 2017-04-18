e32da6106c7c7a52fe7c7dd01c43df8142f646c3
ckobayashi <ckobayashi@konkerlabs.com>
2017-04-18 15:17:14 -0300

    remove TransformationRepository;.findAllByTenantId(String tenantId)

--- konker.registry.services.core/src/main/java/com/konkerlabs/platform/registry/business/repositories/TransformationRepository.java
+++ konker.registry.services.core/src/main/java/com/konkerlabs/platform/registry/business/repositories/TransformationRepository.java
@@ -1,6 +1,5 @@
 package com.konkerlabs.platform.registry.business.repositories;
 
-import com.konkerlabs.platform.registry.business.model.Tenant;
 import com.konkerlabs.platform.registry.business.model.Transformation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -8,18 +7,16 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 
 public interface TransformationRepository extends MongoRepository<Transformation,String> {
-    @Query("{ 'tenant.id' : ?0 }")
-    List<Transformation> findAllByTenantId(String tenantId);
 
     @Query("{ 'tenant.id' : ?0, 'application.name' : ?1 }")
-    List<Transformation> findAllByApplicationId(String tenantId, String applicationId);
+    List<Transformation> findAllByApplicationId(String tenantId, String applicationName);
 
     @Query("{ 'tenant.id' : ?0, 'application.name' : ?1, 'name' : ?2 }")
     List<Transformation> findByName(String tenantId, String applicationId, String name);
 
     @Query("{ 'tenant.id' : ?0, 'guid' : ?1, 'application.name' : ?2 }")
     Transformation findByTenantIdApplicationIdAndTransformationGuid(String id, String guid, String applicationId);
-    
+
     @Query("{  'guid' : ?0 }")
     Transformation findByGuid(String guid);
 }
