c63ec5c68f5a2738368af0718b9d46190d75ac13
dapolina <doliveira.apolinario@gmail.com>
2017-05-03 16:26:23 -0300

    fixed the count in index page to consider all application

--- konker.registry.web/src/main/java/com/konkerlabs/platform/registry/web/controllers/ControlPanelController.java
+++ konker.registry.web/src/main/java/com/konkerlabs/platform/registry/web/controllers/ControlPanelController.java
@@ -1,14 +1,25 @@
 package com.konkerlabs.platform.registry.web.controllers;
 
-import com.konkerlabs.platform.registry.business.model.*;
-import com.konkerlabs.platform.registry.business.services.api.*;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import com.konkerlabs.platform.registry.business.model.Application;
+import com.konkerlabs.platform.registry.business.model.Device;
+import com.konkerlabs.platform.registry.business.model.EventRoute;
+import com.konkerlabs.platform.registry.business.model.RestDestination;
+import com.konkerlabs.platform.registry.business.model.Tenant;
+import com.konkerlabs.platform.registry.business.model.Transformation;
+import com.konkerlabs.platform.registry.business.services.api.ApplicationService;
+import com.konkerlabs.platform.registry.business.services.api.DeviceRegisterService;
+import com.konkerlabs.platform.registry.business.services.api.EventRouteService;
+import com.konkerlabs.platform.registry.business.services.api.RestDestinationService;
+import com.konkerlabs.platform.registry.business.services.api.ServiceResponse;
+import com.konkerlabs.platform.registry.business.services.api.TransformationService;
 
 @Controller
 @Scope("request")
@@ -18,14 +29,14 @@ public class ControlPanelController {
 	@Autowired
 	private Tenant tenant;
 
-    @Autowired
-    private Application application;
-
 	@Autowired
 	private DeviceRegisterService deviceRegisterService;
 
 	@Autowired
 	private EventRouteService eventRouteService;
+	
+	@Autowired
+	private ApplicationService applicationService;
 
 	@Autowired
 	private TransformationService transformationService;
@@ -41,26 +52,31 @@ public class ControlPanelController {
 		int routesCount = 0;
 		int transformationsCount = 0;
 		int restDestinationsCount = 0;
-
-		ServiceResponse<List<Device>> deviceResponse = deviceRegisterService.findAll(tenant, application);
-		if (deviceResponse.isOk()) {
-			devicesCount = deviceResponse.getResult().size();
+		
+		List<Application> applications = applicationService.findAll(tenant).getResult();
+		
+		for (Application app : applications) {
+			ServiceResponse<List<Device>> deviceResponse = deviceRegisterService.findAll(tenant, app);
+			if (deviceResponse.isOk()) {
+				devicesCount += deviceResponse.getResult().size();
+			}
+			
+			ServiceResponse<List<EventRoute>> routesResponse = eventRouteService.getAll(tenant, app);
+			if (routesResponse.isOk()) {
+				routesCount += routesResponse.getResult().size();
+			}
+			
+			ServiceResponse<List<Transformation>> transformationResponse = transformationService.getAll(tenant, app);
+			if (transformationResponse.isOk()) {
+				transformationsCount += transformationResponse.getResult().size();
+			}
+			
+			ServiceResponse<List<RestDestination>> destinationsResponse = restDestinationService.findAll(tenant, app);
+			if (destinationsResponse.isOk()) {
+				restDestinationsCount += destinationsResponse.getResult().size();
+			}
 		}
 
-		ServiceResponse<List<EventRoute>> routesResponse = eventRouteService.getAll(tenant, application);
-		if (routesResponse.isOk()) {
-			routesCount = routesResponse.getResult().size();
-		}
-
-		ServiceResponse<List<Transformation>> transformationResponse = transformationService.getAll(tenant, application);
-		if (transformationResponse.isOk()) {
-			transformationsCount = transformationResponse.getResult().size();
-		}
-
-		ServiceResponse<List<RestDestination>> destinationsResponse = restDestinationService.findAll(tenant, application);
-		if (destinationsResponse.isOk()) {
-			restDestinationsCount = destinationsResponse.getResult().size();
-		}
 
 		mv.addObject("devicesCount", devicesCount);
 		mv.addObject("routesCount", routesCount);
