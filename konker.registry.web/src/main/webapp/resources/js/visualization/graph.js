8373925e8385c6df5420ec94cc670fb84f260f3e
Douglas Apolinario <doliveira.apolinario@gmail.com>
2017-12-27 17:04:45 -0200

    fixed bug whon plot the graphic

--- konker.registry.web/src/main/webapp/resources/js/visualization/graph.js
+++ konker.registry.web/src/main/webapp/resources/js/visualization/graph.js
@@ -4,13 +4,23 @@ function getByPath(o, s) {
     var a = s.split('.');
     for (var i = 0, n = a.length; i < n; ++i) {
         var k = a[i];
-        if (k in o) {
+        if (isObject(o) && k in o) {
             o = o[k];
         } else {
             return;
         }
     }
     return o;
+}
+
+function isObject(value) {
+	if (Array.isArray(value)) {
+		return true;
+	}
+	
+	for (key in value) return true;
+	
+	return false;
 }
 
 var insertLinebreaks = function (d) {
