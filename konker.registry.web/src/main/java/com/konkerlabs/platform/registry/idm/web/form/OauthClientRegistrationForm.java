ffd6d8f51d3ac5bc4e223e4185bbca6c0b953068
ckobayashi <ckobayashi@konkerlabs.com>
2018-08-28 09:32:05 -0300

    add IDM webpages

---
+++ konker.registry.web/src/main/java/com/konkerlabs/platform/registry/idm/web/form/OauthClientRegistrationForm.java
@@ -0,0 +1,49 @@
+package com.konkerlabs.platform.registry.idm.web.form;
+
+import com.konkerlabs.platform.registry.business.model.Application;
+import com.konkerlabs.platform.registry.business.model.Tenant;
+import com.konkerlabs.platform.registry.business.model.OauthClientDetails;
+import com.konkerlabs.platform.registry.idm.web.form.api.ModelBuilder;
+import lombok.Data;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Data
+public class OauthClientRegistrationForm
+        implements ModelBuilder<OauthClientDetails, OauthClientRegistrationForm,Void> {
+
+    private String clientId;
+    private String name;
+    private Application application;
+    private String clientSecret;
+    private Tenant tenant;
+    private Boolean active;
+    private String webServerRedirectUri;
+
+    @Override
+    public OauthClientDetails toModel() {
+        return OauthClientDetails.builder()
+                .name(getName())
+                .application(getApplication())
+                .tenant(getTenant())
+                .active(Optional.ofNullable(getActive()).isPresent() ? getActive() : true)
+                .clientSecret(Optional.ofNullable(
+                        getClientSecret()).isPresent() ?
+                        getClientSecret() : UUID.randomUUID().toString())
+                .webServerRedirectUri(getWebServerRedirectUri())
+                .build();
+    }
+
+    @Override
+    public OauthClientRegistrationForm fillFrom(OauthClientDetails model) {
+        this.setApplication(model.getApplication());
+        this.setClientId(model.getClientId());
+        this.setClientSecret(model.getClientSecret());
+        this.setActive(model.getActive());
+        this.setWebServerRedirectUri(model.getWebServerRedirectUri());
+
+        return this;
+    }
+
+}
