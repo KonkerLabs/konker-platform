1f366510b0f10e25506cb8034efadd3879c4c90e
Andre Rocha <andre.rocha@reclameaqui.com.br>
2018-09-25 20:22:34 -0300

    add k8s to konker-data

--- konker.registry.data/docker-resources/docker-entrypoint.sh
+++ konker.registry.data/docker-resources/docker-entrypoint.sh
@@ -49,8 +49,62 @@ echo "#### username: *****"
 echo "#### password: *****"
 echo "#### host: $REDIS_HOSTNAME"
 echo "#### port: $REDIS_PORT"
+echo "## Redis"
+echo "#### host: $REDIS_HOSTNAME"
+echo "#### port: $REDIS_PORT"
+echo "#### password: ******"
+echo "## Rabbit MQ"
+echo "#### host: $RABBITMQ_HOSTNAME"
+echo "#### vhost: $RABBITMQ_VHOST"
+echo "#### user: *******"
+echo "#### password: *******"
+echo "## SMS"
+echo "#### enabled: $SMS_ENABLED"
+echo "#### uri: $SMS_URI"
+echo "#### username: *******"
+echo "#### password: *******"
+echo "#### from: $SMS_FROM"
 
 /filebeat/filebeat -e -c /filebeat/filebeat.yml &
 /usr/local/sbin/nginx &
-java -Dconfig.file=/var/lib/konker/application.conf -Dmongo.hostname=$MONGODB_HOSTNAME -Dmongo.port=$MONGODB_PORT -Dmongo.username=$MONGODB_USERNAME -Dmongo.password=$MONGODB_PASSWORD -DmongoAudit.hostname=$MONGODB_AUDIT_HOSTNAME -DmongoAudit.port=$MONGODB_AUDIT_PORT -DmongoAudit.username=$MONGODB_AUDIR_USERNAME -DmongoAudit.password=$MONGODB_AUDIT_PASSWORD -Deventstorage.bean=$EVENT_STORAGE -Dcassandra.clustername=$CASSANDRA_CLUSTERNAME -Dcassandra.keyspace=$CASSANDRA_KEYSPACE -Dcassandra.hostname=$CASSANDRA_HOSTNAME -Dcassandra.port=$CASSANDRA_PORT -Dcassandra.username=$CASSANDRA_USERNAME -Dcassandra.password=$CASSANDRA_PASSWORD -Dredis.master.host=$REDIS_HOSTNAME -Dredis.master.port=$REDIS_PORT -Drabbitmq.hostname=$RABBITMQ_HOSTNAME -Drabbitmq.username=$RABBITMQ_USERNAME -Drabbitmq.password=$RABBITMQ_PASSWORD -Drabbitmq.virtualHost=$RABBITMQ_VHOST -jar /var/lib/konker/registry-data.jar
+
+java \ 
+    -Dconfig.file=/var/lib/konker/application.conf \
+    -Dmongo.hostname=$MONGODB_HOSTNAME \
+    -Dmongo.port=$MONGODB_PORT \
+    -Dmongo.username=$MONGODB_USERNAME \
+    -Dmongo.password=$MONGODB_PASSWORD \
+    -DmongoAudit.hostname=$MONGODB_AUDIT_HOSTNAME \
+    -DmongoAudit.port=$MONGODB_AUDIT_PORT \
+    -DmongoAudit.username=$MONGODB_AUDIR_USERNAME \
+    -DmongoAudit.password=$MONGODB_AUDIT_PASSWORD \
+    -Deventstorage.bean=$EVENT_STORAGE \
+    -Dcassandra.clustername=$CASSANDRA_CLUSTERNAME \
+    -Dcassandra.keyspace=$CASSANDRA_KEYSPACE \
+    -Dcassandra.hostname=$CASSANDRA_HOSTNAME \
+    -Dcassandra.port=$CASSANDRA_PORT \
+    -Dcassandra.username=$CASSANDRA_USERNAME \
+    -Dcassandra.password=$CASSANDRA_PASSWORD \
+    -Dredis.master.host=$REDIS_HOSTNAME \
+    -Dredis.master.port=$REDIS_PORT \
+    -Dredis.master.password=$REDIS_PASSWORD \
+    -Drabbitmq.hostname=$RABBITMQ_HOSTNAME \
+    -Drabbitmq.username=$RABBITMQ_USERNAME \
+    -Drabbitmq.password=$RABBITMQ_PASSWORD \
+    -Drabbitmq.virtualHost=$RABBITMQ_VHOST \
+    -Dsms.username=$SMS_ENABLED \
+    -Dsms.uri=$SMS_URI \
+    -Dsms.username=$SMS_USERNAME \
+    -Dsms.password=$SMS_PASSWORD \
+    -Dsms.from=$SMS_FROM \
+    -DpubServer.httpHostname=$PUB_SERVER_HTTP_HOSTNAME \
+    -DpubServer.httpPort=$PUB_SERVER_HTTP_PORT \
+    -DpubServer.httpsPort=$PUB_SERVER_HTTPS_PORT \
+    -DpubServer.mqttHostName=$PUB_SERVER_MQTT_HOSTNAME \
+    -DpubServer.mqttPort=$PUB_SERVER_MQTT_PORT \
+    -DpubServer.mqttTlsPort=$PUB_SERVER_MQTT_TLS_PORT \
+    -DpubServer.httpCtx=$PUB_SERVER_HTTP_CTX \
+    -DpubServer.sslEnabled=$PUB_SERVER_SSL_ENABLED \
+    -jar /var/lib/konker/registry-data.jar
+
 exec "$@"
